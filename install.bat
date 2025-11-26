facd91fd728f8b9055a72f3458e14099d968281b
Erik Lledo <65349896+lledoerik@users.noreply.github.com>
2025-11-26 16:16:08 +0100

    scripts .bat optimitzats

--- install.bat
+++ install.bat
@@ -4,34 +4,34 @@ echo    HERMES - INSTALACIO COMPLETA
 echo ====================================
 echo.
 
-echo [1/4] Creant entorn virtual Python...
+echo [1/5] Creant entorn virtual Python...
 python -m venv venv
 call venv\Scripts\activate
 
-echo [2/4] Instal·lant dependencies Python...
+echo [2/5] Instal·lant dependencies Python...
 pip install --upgrade pip
 pip install -r requirements.txt
 python.exe -m pip install --upgrade pip
 
-echo [3/4] Instal·lant dependencies React...
+echo [3/5] Instal·lant dependencies React...
 cd frontend
 call npm install
 cd ..
 
-echo [4/4] Creant directoris...
+echo [4/5] Creant directoris...
 if not exist "storage" mkdir storage
 if not exist "storage\cache" mkdir storage\cache
 if not exist "storage\metadata" mkdir storage\metadata
+
+
+echo [5/5] Escannejant multimedia...
+./scan.bat
 
 echo.
 echo ====================================
 echo    INSTALACIO COMPLETADA!
 echo ====================================
 echo.
-echo Segueix aquests passos:
-echo.
-echo 1. Edita config\settings.py amb les teves rutes
-echo 2. Executa: start-scan.bat per escanejar
-echo 3. Executa: start-all.bat per iniciar
+echo Executa: start.bat per iniciar
 echo.
 pause
